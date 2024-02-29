38bebc8d0de632d15dc6593abb15fedaa3de6d36
iymorshe <92063753+iymorshe@users.noreply.github.com>
2024-02-28 16:43:02 -0800

    Initial Commit

---
+++ SpatialQuran/SpatialQuranApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialQuranApp.swift
+//  SpatialQuran
+//
+//  Created by Iman Morshed on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SpatialQuranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
