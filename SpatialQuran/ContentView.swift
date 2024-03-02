fb7249ca14cd52d3157d43e640c5a6357ba62551
iymorshe <92063753+iymorshe@users.noreply.github.com>
2024-03-02 15:33:43 -0800

    x

--- SpatialQuran/ContentView.swift
+++ SpatialQuran/ContentView.swift
@@ -10,23 +10,33 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
-    @State var surahName: String = "The Opening"
+    @State var surahNumber: Int = 1
     @State var quranText: [Ayah] = []
     @StateObject var quran: Quran = Quran.shared
     var body: some View {
         VStack {
             HStack {
                 Button {
-                    print("hello")
+                    if surahNumber > 1 {
+                        surahNumber -= 1
+                        Task {
+                            await loadVersesForSurah(surahNumber: surahNumber)
+                        }
+                    }
+                    
                 } label: {
                     Image(systemName: "arrow.left")
                         .resizable()
                         .frame(width: 30, height: 30)
                 }
-                Text(surahName)
+                Text(englishSurahNames[surahNumber])
                     .font(.title)
+                    .fontWeight(.bold)
                 Button {
-                    print("hello")
+                    surahNumber += 1
+                    Task {
+                        await loadVersesForSurah(surahNumber: surahNumber)
+                    }
                 } label: {
                     Image(systemName: "arrow.right")
                         .resizable()
@@ -34,18 +44,21 @@ struct ContentView: View {
                 }
             }
             ScrollView {
-                ForEach(quranText, id: \.self) { ayah in
-                    Text("\(ayah.surahNumber).\(ayah.ayahNumber) \(ayah.englishTranslation)")
-                        .padding(10)
-                    if ayah.englishTranslation.contains("way") {
-                        Model3D(named: "Scene", bundle: realityKitContentBundle)
-                            .padding(.bottom, 50)
+                VStack(alignment: .leading){
+                    ForEach(quranText, id: \.self) { ayah in
+                        Text("\(ayah.ayahNumber). \(ayah.englishTranslation)")
+                            .font(.system(size:30))
+                        Spacer()
+                        //                    if ayah.englishTranslation.contains("way") {
+                        //                        Model3D(named: "Scene", bundle: realityKitContentBundle)
+                        //                            .padding(.bottom, 50)
+                        //                    }
                     }
                 }
             }
             .onAppear {
                 Task {
-                    await loadVersesForSurah(surahNumber: 1)
+                    await loadVersesForSurah(surahNumber: surahNumber)
                 }
             }
         }
@@ -53,12 +66,11 @@ struct ContentView: View {
     private func loadVersesForSurah(surahNumber: Int) async {
         guard surahNumber - 1 < versesPerChapter.count else { return }
         let numberOfVerses = versesPerChapter[surahNumber]
-        var surah = Surah(surahNumber: surahNumber)
+        quranText = []
         for verseNumber in 1...numberOfVerses {
             do {
                 let ayah = try await fetchVerse(surahNumber: surahNumber, verseNumber: verseNumber)
                 DispatchQueue.main.async {
-                    surah.ayahs.append(ayah)
                     quranText.append(ayah)
                 }
                 
